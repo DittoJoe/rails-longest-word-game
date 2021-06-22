f9bffe29f603d861adc0de9cd86595d0090817be
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-06-22 12:02:20 +0200

    routed root to new game page

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,26 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    grid = []
+    10.times { grid << ('A'..'Z').to_a.sample }
+    @letters = grid
+  end
+
+  def score
+    @word = params[:word]
+    @letters = params[:letters]
+    @score = 0
+    json = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{@word}").read)
+    if @word.upcase.chars.all? { |l| @word.upcase.chars.count(l) <= @letters.count(l) }
+      if json['found']
+        @score = @word.length * 10
+        @message = 'Grattis!'
+      else
+        @message = "We couldn't find that word, sorry!"
+      end
+    else
+      @message = 'Oops...looks like you used letters not on the board!'
+    end
+  end
+end
